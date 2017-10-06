1bf25d003edd7cc4378dd76d4d4a98f491798ba1
William Durkin <github@williamdurkin.com>
2017-10-06 15:53:35 +0200

    extract some metrics from query plans for easier analysis. closes #31

--- setup/install_gather_statistics.sql
+++ setup/install_gather_statistics.sql
@@ -1,7 +1,7 @@
 /*********************************************************************************************
 Open Query Store
 Install gather_statistics for Open Query Store
-v0.5 - October 2017
+v0.6 - October 2017
 
 Copyright:
 William Durkin (@sql_williamd) / Enrico van de Laar (@evdlaar)
@@ -112,7 +112,11 @@ AS
                                               [plan_sizeinbytes],
                                               [plan_type],
                                               [plan_objecttype],
-                                              [plan_executionplan]
+                                              [plan_executionplan],
+                                              [estimated_available_degree_of_parallelism],
+                                              [estimated_statement_subtree_cost],
+                                              [estimated_available_memory_grant],
+                                              [cost_threshold_for_parallelism]
                                           )
                             SELECT SUBSTRING( [master].[sys].[fn_repl_hash_binary]( CONVERT( varbinary (MAX), [n].[query]( '.' ))), 1, 32 ),
                                    [cp].[plan_handle],
@@ -123,9 +127,13 @@ AS
                                    [cp].[size_in_bytes],
                                    [cp].[cacheobjtype],
                                    [cp].[objtype],
-                                   [qp].[query_plan]
-                            FROM   [oqs].[plan_dbid]                                            AS [pd]
-                                   INNER JOIN [sys].[dm_exec_cached_plans]                      AS [cp] ON [pd].[plan_handle] = [cp].[plan_handle]
+                                   [qp].[query_plan],
+                                   [plan_executionplan].[value]( '(/ShowPlanXML/BatchSequence/Batch/Statements/StmtSimple/QueryPlan/OptimizerHardwareDependentProperties/@EstimatedAvailableDegreeOfParallelism)[1]', 'int' ) AS [available_degree_of_parallelism],
+                                   CAST([plan_executionplan].[value]( '(/ShowPlanXML/BatchSequence/Batch/Statements/StmtSimple/@StatementSubTreeCost)[1]', 'real' ) AS numeric (19, 12))                                      AS [estimated_statement_subtree_cost],
+                                   [plan_executionplan].[value]( '(/ShowPlanXML/BatchSequence/Batch/Statements/StmtSimple/QueryPlan/OptimizerHardwareDependentProperties/@EstimatedAvailableMemoryGrant)[1]', 'int' )         AS [estimated_available_memory_grant],
+                                   ( SELECT CAST([C].[value_in_use] AS int) FROM   [sys].[configurations] AS [C] WHERE  [C].[name] = 'cost threshold for parallelism' )                                                       AS [cost_threshold_for_parallelism]
+                            FROM   [oqs].[plan_dbid] AS [pd]
+                                   INNER JOIN [sys].[dm_exec_cached_plans] AS [cp] ON [pd].[plan_handle] = [cp].[plan_handle]
                                    CROSS APPLY [sys].[dm_exec_query_plan]( [cp].[plan_handle] ) AS [qp]
                                    CROSS APPLY [query_plan].[nodes]( '/ShowPlanXML/BatchSequence/Batch/Statements/StmtSimple/QueryPlan/RelOp' ) AS [q]([n])
                                    CROSS APPLY [sys].[dm_exec_sql_text]( [cp].[plan_handle] )
@@ -145,8 +153,8 @@ AS
                         VALUES ( @log_logrunid, GETDATE(), 'OpenQueryStore captured ' + CONVERT( varchar, @log_newplans ) + ' new plan(s)...' );
                     END
 
-                    -- Grab all of the queries (statement level) that are connected to the plans inside the OQS
-                    ;
+                -- Grab all of the queries (statement level) that are connected to the plans inside the OQS
+                ;
                 WITH [CTE_Queries] ( [plan_id], [plan_handle], [plan_MD5] )
                 AS ( SELECT [plan_id], [plan_handle], [plan_MD5] FROM [oqs].[plans] )
                 INSERT INTO [oqs].[queries] (   [plan_id],
@@ -170,10 +178,10 @@ AS
                                    [qs].[statement_start_offset],
                                    [qs].[statement_end_offset],
                                    [qs].[creation_time]
-                            FROM   [CTE_Queries]                                             AS [cte]
-                                   INNER JOIN [oqs].[query_stats]                            AS [qs] ON [cte].[plan_handle] = [qs].[plan_handle]
+                            FROM   [CTE_Queries] AS [cte]
+                                   INNER JOIN [oqs].[query_stats] AS [qs] ON [cte].[plan_handle] = [qs].[plan_handle]
                                    CROSS APPLY [sys].[dm_exec_sql_text]( [qs].[sql_handle] ) AS [st]
-                            WHERE  ( [cte].[plan_MD5] + [qs].[query_hash] ) NOT IN ( SELECT [query_plan_MD5] FROM [oqs].[queries] );
+                            WHERE  ( [cte].[plan_MD5] + [qs].[query_hash] ) NOT IN ( SELECT [Query_plan_MD5] FROM [oqs].[queries] );
 
                 SET @log_newqueries = @@RowCount;
 
@@ -264,7 +272,7 @@ AS
                                    [qs].[min_logical_writes],
                                    [qs].[max_logical_writes],
                                    0
-                            FROM   [oqs].[queries]                AS [oqs_q]
+                            FROM   [oqs].[queries] AS [oqs_q]
                                    INNER JOIN [oqs].[query_stats] AS [qs] ON (   [oqs_q].[query_hash] = [qs].[query_hash]
                                                                                  AND [oqs_q].[query_statement_start_offset] = [qs].[statement_start_offset]
                                                                                  AND [oqs_q].[query_statement_end_offset] = [qs].[statement_end_offset]
@@ -323,8 +331,8 @@ AS
                         DROP TABLE [#OQS_Wait_Stats];
                     END
 
-                    -- Calculate Deltas for Runtime stats
-                    ;
+                -- Calculate Deltas for Runtime stats
+                ;
                 WITH [CTE_Update_Runtime_Stats] ( [query_id], [interval_id], [execution_count], [total_elapsed_time], [total_rows], [total_worker_time], [total_physical_reads], [total_logical_reads], [total_logical_writes] )
                 AS ( SELECT [QRS].[query_id],
                             [QRS].[interval_id],
@@ -354,7 +362,7 @@ AS
                        ( [qrs].[total_logical_writes] - [cte].[total_logical_writes] )                                                                                  AS [Delta Total Log Writes],
                        ISNULL((( [qrs].[total_logical_writes] - [cte].[total_logical_writes] ) / NULLIF(( [qrs].[execution_count] - [cte].[execution_count] ), 0)), 0 ) AS [Avg. Log writes]
                 INTO   [#OQS_Runtime_Stats]
-                FROM   [CTE_Update_Runtime_Stats]             AS [cte]
+                FROM   [CTE_Update_Runtime_Stats] AS [cte]
                        INNER JOIN [oqs].[query_runtime_stats] AS [qrs] ON [cte].[query_id] = [qrs].[query_id]
                 WHERE  [qrs].[interval_id] = ( SELECT MAX( [interval_id] ) FROM [oqs].[intervals] );
 
@@ -391,7 +399,7 @@ AS
                        [qrs].[avg_logical_reads] = [tqrs].[Avg. Log reads],
                        [qrs].[total_logical_writes] = [tqrs].[Delta Total Log Writes],
                        [qrs].[avg_logical_writes] = [tqrs].[Avg. Log writes]
-                FROM   [oqs].[query_runtime_stats]     AS [qrs]
+                FROM   [oqs].[query_runtime_stats] AS [qrs]
                        INNER JOIN [#OQS_Runtime_Stats] AS [tqrs] ON (   [qrs].[interval_id] = [tqrs].[interval_id]
                                                                         AND [qrs].[query_id] = [tqrs].[query_id]
                                                                     );
@@ -403,7 +411,7 @@ AS
                     END;
 
                     -- Calculate Deltas for Wait stats
-				;
+                ;
                 WITH [CTE_Update_wait_Stats] ( [interval_id], [wait_type], [waiting_tasks_count], [wait_time_ms], [max_wait_time_ms], [signal_wait_time_ms] )
                     AS ( SELECT [WS].[interval_id],
                                 [WS].[wait_type],
@@ -421,7 +429,7 @@ AS
                        ( [WS].[max_wait_time_ms] - [cte].[max_wait_time_ms] )       AS [Delta Max Wait Time ms],
                        ( [WS].[signal_wait_time_ms] - [cte].[signal_wait_time_ms] ) AS [Delta Signal Wait Time ms]
                 INTO   [#OQS_Wait_Stats]
-                FROM   [CTE_Update_wait_Stats]       AS [cte]
+                FROM   [CTE_Update_wait_Stats] AS [cte]
                        INNER JOIN [oqs].[wait_stats] AS [WS] ON [cte].[wait_type] = [WS].[wait_type]
                 WHERE  [WS].[interval_id] = ( SELECT MAX( [interval_id] ) FROM [oqs].[intervals] );
 
@@ -442,7 +450,6 @@ AS
                         SELECT * FROM [#OQS_Wait_Stats];
                     END;
 
-
                 -- Update the runtime statistics of the queries captured in the previous interval
                 -- with the delta runtime information
                 UPDATE [WS]
@@ -450,7 +457,7 @@ AS
                        [WS].[wait_time_ms] = [Delta Wait Time ms],
                        [WS].[max_wait_time_ms] = [Delta Max Wait Time ms],
                        [WS].[signal_wait_time_ms] = [Delta Signal Wait Time ms]
-                FROM   [oqs].[wait_stats]           AS [WS]
+                FROM   [oqs].[wait_stats] AS [WS]
                        INNER JOIN [#OQS_Wait_Stats] AS [tws] ON (   [WS].[interval_id] = [tws].[interval_id]
                                                                     AND [WS].[wait_type] = [tws].[wait_type]
                                                                 );
