1b4439ef42a2bb4062023469dd8ea3ecedda13c9
Rob Sewell <mrrobsewell@outlook.com>
2017-10-30 08:39:33 -0700

    tests for schedulertype paramter

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -9,7 +9,7 @@ if ((Split-Path $ModuleBase -Leaf) -eq 'Tests') {
 ## this variable is for the VSTS tasks and is to be used for refernecing any mock artifacts
 $Env:ModuleBase = $ModuleBase
 Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop | Out-Null
-Describe "Basic function unit tests" -Tags Build , Unit {
+Describe "Basic function unit tests" -Tags Build , Unit{
 
 }
 InModuleScope -ModuleName $ModuleName -ScriptBlock {
@@ -18,14 +18,14 @@ InModuleScope -ModuleName $ModuleName -ScriptBlock {
             Get-Command Install-OpenQueryStore -ErrorAction SilentlyContinue | Should Not BE NullOrEmpty
         }
         Context "Install-OpenQueryStore input" {
-            function Invoke-Catch {}
+            function Invoke-Catch{}
             Mock Invoke-Catch {}
             $results = (Get-Content $PSScriptRoot\json\SQL2012version.json) -join "`n" | ConvertFrom-Json 
             Mock Connect-DbaInstance {$results}
             Mock Get-DbaDatabase -ParameterFilter { $Database -and $Database -eq "WrongDatabase" } {}
             Mock Get-DbaDatabase -ParameterFilter { $Database -and $Database -eq "OQSDatabase" } {'A Database'}
-            It "Should call the Invoke-Catch if there is no database" {
-                Install-OpenQueryStore -SqlInstance Dummy -Database WrongDatabase
+            It "Should call the Invoke-Catch if there is no database"{
+                Install-OpenQueryStore -SqlInstance Dummy -Database WrongDatabase -SchedulerType 'Service Broker'
                 $assertMockParams = @{
                     'CommandName' = 'Invoke-Catch'
                     'Times'       = 1
@@ -34,7 +34,7 @@ InModuleScope -ModuleName $ModuleName -ScriptBlock {
                 Assert-MockCalled @assertMockParams 
             }
             It "Should Not call Invoke-Catch if there is a database" {
-                Install-OpenQueryStore -SqlInstance Dummy -Database OQSDatabase
+                Install-OpenQueryStore -SqlInstance Dummy -Database OQSDatabase -SchedulerType 'Service Broker'
                 $assertMockParams = @{
                     'CommandName' = 'Invoke-Catch'
                     'Times'       = 1
@@ -44,40 +44,46 @@ InModuleScope -ModuleName $ModuleName -ScriptBlock {
             
             }
             It "Should accept a value for OQSMode of classic" {
-                {Install-OpenQueryStore -SqlInstance Dummy -DatabaseName OQSDatabase -OQSMode 'Classic'} | Should Not Throw
+                {Install-OpenQueryStore -SqlInstance Dummy -DatabaseName OQSDatabase -OQSMode 'Classic'-SchedulerType 'Service Broker'} | Should Not Throw
             }
             
             It "Should accept a value for OQS Mode of centralized" {
-                {Install-OpenQueryStore -SqlInstance Dummy -DatabaseName OQSDatabase -OQSMode 'Centralized'} | Should Not Throw
+                {Install-OpenQueryStore -SqlInstance Dummy -DatabaseName OQSDatabase -OQSMode 'Centralized' -SchedulerType 'Service Broker'} | Should Not Throw
+            }            
+            It "Should accept a value for SchedulerType of Service Broker" {
+                {Install-OpenQueryStore -SqlInstance Dummy -DatabaseName OQSDatabase -OQSMode 'Centralized' -SchedulerType 'Service Broker' } | Should Not Throw
+            }            
+            It "Should accept a value for SchedulerType of SQL Agent" {
+                {Install-OpenQueryStore -SqlInstance Dummy -DatabaseName OQSDatabase -OQSMode 'Centralized' -SchedulerType 'SQL Agent' } | Should Not Throw
             }            
         }
         Context "Version Support" {
-            function Invoke-Catch {}
+            function Invoke-Catch{}
             Mock Invoke-Catch {break}
             #We only support between SQL Server 2008 (v10.X.X) and SQL Server 2014
             It "Should Break for SQL 2017" {
                 $results = (Get-Content $PSScriptRoot\json\SQL2017version.json) -join "`n" | ConvertFrom-Json 
                 Mock Connect-DbaInstance {$results}
                 
-                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy | Should Be 
+                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy -SchedulerType 'Service Broker'| Should Be 
             }
             It "Should Break for SQL 2016" {
                 $results = (Get-Content $PSScriptRoot\json\SQL2016version.json) -join "`n" | ConvertFrom-Json 
                 Mock Connect-DbaInstance {$results}
                 
-                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy | Should Be 
+                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy -SchedulerType 'Service Broker' | Should Be 
             }
             It "Should Break for SQL 2005" {
                 $results = (Get-Content $PSScriptRoot\json\SQL2005version.json) -join "`n" | ConvertFrom-Json 
                 Mock Connect-DbaInstance {$results}
                 
-                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy | Should Be 
+                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy -SchedulerType 'Service Broker' | Should Be 
             }
             It "Should Break for SQL 2000" {
                 $results = (Get-Content $PSScriptRoot\json\SQL2000version.json) -join "`n" | ConvertFrom-Json 
                 Mock Connect-DbaInstance {$results}
                 
-                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy | Should Be 
+                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy -SchedulerType 'Service Broker'| Should Be 
             }
             It 'Checks the Mock was called for Connect-DbaInstance' {
                 $assertMockParams = @{
@@ -99,8 +105,8 @@ InModuleScope -ModuleName $ModuleName -ScriptBlock {
         Context "Install-OpenQueryStore Execution" {
             Mock Get-Module {}
             Mock Write-Warning {}
-            It "Should write a warning if dbatools module not available" {      
-                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy | Should Not Throw
+            It "Should write a warning if dbatools module not available"{      
+                Install-OpenQueryStore -SqlInstance Dummy -Database Dummy -SchedulerType 'Service Broker'| Should Not Throw
             }
             It 'Checks the Mock was called for Write-Warning' {
                 $assertMockParams = @{
